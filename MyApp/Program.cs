901c391f492c3d8e1540f7325da02c79f6867223
bstudtma <bstudtma@gmail.com>
2025-08-23 14:54:46 -0500

    Add MyApp project and initial implementation

---
+++ MyApp/Program.cs
@@ -0,0 +1,14 @@
+﻿// <copyright file="Program.cs" company="BARS">
+// Copyright (c) BARS. All rights reserved.
+// </copyright>
+using SimConnect.NET;
+
+var client = new SimConnectClient();
+await client.ConnectAsync();
+
+// Get aircraft data
+var altitude = await client.SimVars.GetAsync<double>("PLANE ALTITUDE", "feet");
+var airspeed = await client.SimVars.GetAsync<double>("AIRSPEED INDICATED", "knots");
+
+Console.WriteLine($"Altitude: {altitude:F0} ft");
+Console.WriteLine($"Airspeed: {airspeed:F0} kts");
