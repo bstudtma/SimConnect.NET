660d72c1ff603b16c099cc5c36a14673e489cfb2
bstudtma <bstudtma@gmail.com>
2025-08-24 07:57:55 -0500

    Update aircraft data retrieval to use GetAsync method and restore output formatting

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -7,9 +7,10 @@ var client = new SimConnectClient();
 await client.ConnectAsync();
 
 // Get aircraft data in one shot using a SimVar-annotated struct
-// var snapshot = await client.SimVars.GetAsync<MyApp.AircraftSnapshot>();
-// Console.WriteLine($"Altitude: {snapshot.AltitudeFeet:F0} ft");
-// Console.WriteLine($"Airspeed: {snapshot.IndicatedAirspeedKnots:F0} kts");
-var snapshot = await client.SimVars.Test<MyApp.AircraftSnapshot>();
-Console.WriteLine($"[Test] Altitude: {snapshot.AltitudeFeet:F0} ft");
-Console.WriteLine($"[Test] Airspeed: {snapshot.IndicatedAirspeedKnots:F0} kts");
+var snapshot = await client.SimVars.GetAsync<MyApp.AircraftSnapshot>();
+Console.WriteLine($"Altitude: {snapshot.AltitudeFeet:F0} ft");
+Console.WriteLine($"Airspeed: {snapshot.IndicatedAirspeedKnots:F0} kts");
+
+// var snapshot = await client.SimVars.Test<MyApp.AircraftSnapshot>();
+// Console.WriteLine($"[Test] Altitude: {snapshot.AltitudeFeet:F0} ft");
+// Console.WriteLine($"[Test] Airspeed: {snapshot.IndicatedAirspeedKnots:F0} kts");
